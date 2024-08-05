d48d26dd3dfef21a06b31dff0bd7f362d83b60ef
saymanq <87442765+saymanq@users.noreply.github.com>
2024-08-05 06:14:42 +0300

    Changes made

--- inventory-management-app/src/app/page.tsx
+++ inventory-management-app/src/app/page.tsx
@@ -100,7 +100,7 @@ export default function Home() {
 
   if (!user) {
     return <Box
-    width="100vw"
+    width="100%"
     height="100%"
     display={'flex'}
     justifyContent={'center'}
@@ -115,7 +115,7 @@ export default function Home() {
 
   return (
   <Box
-    width="100vw"
+    width="100%"
     height="100%"
     display={'flex'}
     justifyContent={'center'}

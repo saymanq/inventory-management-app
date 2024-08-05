58d530423850054e1e796bb8d1a6cfe9315f3840
saymanq <87442765+saymanq@users.noreply.github.com>
2024-08-05 06:34:11 +0300

    Fixed deployment issues

--- inventory-management-app/src/firebase.js
+++ inventory-management-app/src/firebase.js
@@ -21,6 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const auth = getAuth(app);
 export { firestore, auth };
